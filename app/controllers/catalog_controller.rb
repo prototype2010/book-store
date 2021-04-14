25d1f75c18ca32df145fb170bbe3720605217050
prototype2010 <Firstday2010>
2021-04-14 12:25:34 +0300

    models and migrations

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,2 +1,4 @@
 class CatalogController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
   get '/home', to: "home#index"
+  get '/catalog', to: "catalog#index"
 end
