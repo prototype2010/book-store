7449416c9334f1915e118fb5e57e438afc237ce7
prototype2010 <Firstday2010>
2021-04-17 21:10:00 +0300

    basic footer tests

--- spec/features/homes_spec.rb
+++ spec/features/homes_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Home Page', type: :feature do
   let(:home_page) { HomesPage.new }
   let(:navbar) { home_page.navbar }
+  let(:footer) { home_page.footer }
 
   before { home_page.load }
 
@@ -18,5 +19,14 @@ RSpec.describe 'Home Page', type: :feature do
     expect(navbar).to have_content(I18n.t('nav.mobile_development'))
     expect(navbar).to have_content(I18n.t('nav.photo'))
     expect(navbar).to have_content(I18n.t('nav.web_design'))
+  end
+
+  it 'footer contains lints' do
+    home_page.footer.click
+
+    expect(footer).to have_content(I18n.t('generic.shop'))
+    expect(footer).to have_content(I18n.t('generic.home'))
+    expect(footer).to have_content(I18n.t('footer.orders'))
+    expect(footer).to have_content(I18n.t('footer.settings'))
   end
 end
--- spec/support/pages/home_page.rb
+++ spec/support/pages/home_page.rb
@@ -2,4 +2,5 @@ class HomesPage < SitePrism::Page
   set_url '/home'
 
   element :navbar, '#navbar'
+  element :footer, 'footer'
 end
