fcab2eaf86ac6d6cd7a8483d3dda97b63117a6cf
prototype2010 <Firstday2010>
2021-04-20 16:09:41 +0300

    category logic

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,4 +1,6 @@
 module BooksHelper
+  All = 'All'
+
   def book_authors(book)
     book.authors
         .map { |author| "#{author.first_name} #{author.last_name}" }
@@ -6,6 +8,27 @@ module BooksHelper
   end
 
   def books_categories
+    categories = Category.all.map do |category|
+      {
+        name: category.name,
+        display_name: category.name,
+        books_count: category.books_count
+      }
+    end
 
+    total_books = categories
+                    .map { |category| category[:books_count] }
+                    .sum
+
+    categories.unshift({
+                  name: nil,
+                  display_name: All,
+                  books_count: total_books
+                })
   end
+
+  def to_i18_key(string)
+    string.nil? ? All.downcase : string.gsub(' ','_').underscore
+  end
+
 end
