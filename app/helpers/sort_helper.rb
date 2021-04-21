17d772b7524933e5168fdf789738c476bf5919dc
prototype2010 <Firstday2010>
2021-04-21 11:08:19 +0300

    sorting logic

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -7,9 +7,13 @@ module SortHelper
     price_high_low: {sort: 'price', direction: 'DESC', display_text: "Price: High to low"},
   }
 
+  def default_sort
+    BOOKS_SORT[DEFAULT_SORT.to_sym]
+  end
+
   def sort_details(sort = DEFAULT_SORT)
     sort_type = sort.nil? ? DEFAULT_SORT : sort
-    return BOOKS_SORT[DEFAULT_SORT.to_sym] unless BOOKS_SORT.key?(sort_type.to_sym)
+    return default_sort unless BOOKS_SORT.key?(sort_type.to_sym)
 
     BOOKS_SORT[sort_type.to_sym]
   end
